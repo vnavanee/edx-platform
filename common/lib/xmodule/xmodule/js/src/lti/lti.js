e2042a6e0c000364bbae5aa986d4bccba072c360
Alexander Kryklia <kryklia@gmail.com>
2013-08-20 17:51:45 +0300

    updates

---
+++ common/lib/xmodule/xmodule/js/src/lti/lti.js
@@ -0,0 +1,3 @@
+window.LTI = function (html_id){
+	return {};
+}
--- common/lib/xmodule/xmodule/lti_module.py
+++ common/lib/xmodule/xmodule/lti_module.py
@@ -1,46 +1,42 @@
 """
-Graphical slider tool module is ungraded xmodule used by students to
-understand functional dependencies.
+Module that allows to insert LTI tools to page.
 """
 
-import json
 import logging
-from lxml import etree
-from lxml import html
-import xmltodict
+import requests
 
 from xmodule.editing_module import MetadataOnlyEditingDescriptor
-from xmodule.xml_module import XmlDescriptor
 from xmodule.x_module import XModule
-from xmodule.stringify import stringify_children
 from pkg_resources import resource_string
 from xblock.core import String, Scope
 
 log = logging.getLogger(__name__)
 
+
 class LTIFields(object):
+    """provider_url and tool_id together is unique location of LTI in the web."""
+    client_key = String(help="Client key", default='', scope=Scope.settings)
+    client_secret = String(help="Client secret", default='', scope=Scope.settings)
     provider_url = String(
         help="URL of the tool provider",
         default='',
         # TODO: Write a letter.
-        scope=Scope.content
+        scope=Scope.settings
     )
     tool_id = String(
         help="The ID of the tool",
         default="",
-
         # TODO: Write a letter.
-        scope=Scope.content        
+        scope=Scope.settings
     )
 
 
 class LTIModule(LTIFields, XModule):
-    ''' LTI Module
-    '''
+    '''LTI Module'''
 
     js = {
         'js': [
-            resource_string(__name__, 'js/src/lti/main.js')
+            resource_string(__name__, 'js/src/lti/lti.js')
         ]
     }
     css = {'scss': [resource_string(__name__, 'css/gst/display.scss')]}
@@ -49,14 +45,10 @@ class LTIModule(LTIFields, XModule):
     def get_html(self):
         """ Renders parameters to template. """
 
-        # these 3 will be used in class methods
-        self.html_id = self.location.html_id()
-        self.html_class = self.location.category
-
         params = {
             'lti_html': self.get_lti_html(),
-            'element_id': self.html_id,
-            'element_class': self.html_class,
+            'element_id': self.location.html_id(),
+            'element_class': self.location.category,
         }
         content = self.system.render_template(
             'lti.html', params
@@ -64,9 +56,23 @@ class LTIModule(LTIFields, XModule):
         return content
 
     def get_lti_html(self):
-        return "<div>Hello, world!</div>"
+        """Obtains LTI html from provider"""
+        current_user = u'EdX Student'
+        test_url = u'https://bc-staging.vitalsource.com/books/L-999-70103'
+        key = '5c6b9020-066a-11e3-8ffd-0800200c9a66'
+        secret = '75413f322a7b4e76223e332c625f3d34'
+        authorization = requests.auth.OAuth1(
+            client_key=unicode(key),
+            client_secret=unicode(secret)
+        )
+        request = requests.post(
+            url=test_url,
+            auth=authorization,
+            data={u'user_id': current_user})
+        return request.content
+
 
 class LTIModuleDescriptor(LTIFields, MetadataOnlyEditingDescriptor):
     module_class = LTIModule
 
-   
+
