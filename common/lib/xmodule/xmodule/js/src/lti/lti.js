927111f6bdff16ee121fef1af8ae777505dc8ab1
Valera Rozuvan <valera@edx.org>
2013-08-22 11:04:39 +0300

    Rewrite of LTI template. Moved all JavaScript to JS file.

--- common/lib/xmodule/xmodule/js/src/lti/lti.js
+++ common/lib/xmodule/xmodule/js/src/lti/lti.js
@@ -1,3 +1,20 @@
-window.LTI = function (html_id){
-	return {};
+window.LTI = function (element){
+    var form = element.find('#ltiLaunchForm'),
+        frame = element.find('#ltiLaunchFrame');
+
+    console.log('form = ', form);
+    console.log('frame = ', frame);
+
+    $(document).ready(init);
+
+    return;
+
+	function init() {
+        console.log('We are in window.LTI function');
+        console.log('element = ', element);
+
+        form.submit();
+
+        frame.width(400).height(600);
+    }
 }
