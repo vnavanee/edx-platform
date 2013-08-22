a6594a21257f53e1c925fad20cef19298a806b20
Carlos Andrés Rocha <rocha@edx.org>
2013-08-22 14:54:02 -0400

    wip - mongo backend

--- common/djangoapps/track/backends/mongodb.py
+++ common/djangoapps/track/backends/mongodb.py
@@ -11,13 +11,12 @@ log = logging.getLogger('track.backends.mongo')
 
 
 class MongoBackend(BaseBackend):
-
     def __init__(self, **options):
         super(MongoBackend, self).__init__(**options)
 
-        uri = self._make_uri(options)
+        uri = _make_mongodb_uri(options)
         db_name = options.pop('database', 'track')
-        collection_name = options.pop('database', 'events')
+        collection_name = options.pop('collection', 'events')
 
         # By default disable write acknoledgements
         write_concern = options.pop('w', 0)
@@ -27,24 +26,29 @@ class MongoBackend(BaseBackend):
 
         self._create_indexes()
 
-    def _make_uri(self, options):
-        """Make a MongoDB URI from options"""
-
-        host = options.pop('host', 'localhost')
-        port = options.pop('port', 27017)
-        user = options.pop('user', '')
-        password = options.pop('password', '')
-
-        uri = 'mongodb://'
-        if user or password:
-            uri += '{user}:{password}@'.format(user, password)
-        uri += '{host}:{port}'.format(host, port)
-
-        return uri
-
     def _create_indexes(self):
         self.collection.create_index('event_type')
         self.collection.create_index([('time', pymongo.DESCENDING)])
 
     def send(self, event):
         self.collection.insert(event)
+
+
+def _make_mongodb_uri(options):
+    """
+    Make a MongoDB URI from options
+
+    Returns the joined URI and removes the used keys from `options`.
+    """
+
+    host = options.pop('host', 'localhost')
+    port = options.pop('port', 27017)
+    user = options.pop('user', '')
+    password = options.pop('password', '')
+
+    uri = 'mongodb://'
+    if user or password:
+        uri += '{user}:{password}@'.format(user=user, password=password)
+    uri += '{host}:{port}'.format(host=host, port=port)
+
+    return uri
